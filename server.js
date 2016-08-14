4703c526df570e6e9d6aedb29842bdf025289c01
ruchit rami <ruchitrami@gmail.com>
2016-08-14 16:11:35 +0530

    Testing code for socket

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 // var request = require('request');
+var express = require('express');
+var app = express();
 
 
 var connector = new builder.ChatConnector({
@@ -9,6 +11,25 @@ var connector = new builder.ChatConnector({
 });
 
 var bot = new builder.UniversalBot(connector);
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // Setup Restify Server
 var server = restify.createServer();
